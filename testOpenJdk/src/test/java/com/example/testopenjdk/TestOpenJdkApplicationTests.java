d63ef784f7d5c9a62087ca298fe36656564f7091
gh1ndri <leith.ghandri@gmail.com>
2023-04-25 22:20:26 +0100

    create root pipeline

---
+++ testOpenJdk/src/test/java/com/example/testopenjdk/TestOpenJdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testopenjdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenJdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
